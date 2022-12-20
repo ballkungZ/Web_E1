c86fef25a5f4909d9c225015c8d9a2da9f55c6fc
ballkungZ <ballgg12@hotmail.com>
2022-12-20 20:53:58 +0700

    แก้ code

--- Web_Error.js
+++ Web_Error.js
@@ -7,3 +7,9 @@ app.get('/',(req,res)=>{
 app.listen(3000,()=>{
     console.log("Start Server at Port 3000")
 })
+app.get('*',(req,res)=>{
+    res.send('Error 404')
+})
+app.get('/product',function(req,res){
+    res.sendFile(__dirname + '/Ws3.html')
+})
