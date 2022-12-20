9a8f24221d86fe1ca7342e14e4cbd1e6e2f44a6e
ballkungZ <ballgg12@hotmail.com>
2022-12-20 21:15:55 +0700

    แก้ + เพิ่มหน้า

--- Web_Error.js
+++ Web_Error.js
@@ -1,15 +1,17 @@
 const express = require('express')
 const app = express()
+const worker = require('./')
 
 app.get('/',(req,res)=>{
     res.send('Hello World from Express FrameWork')
 })
-app.listen(3000,()=>{
-    console.log("Start Server at Port 3000")
-})
-app.get('*',(req,res)=>{
-    res.send('Error 404')
-})
 app.get('/product',function(req,res){
     res.sendFile(__dirname + '/Ws3.html')
+})
+app.get('*',(req,res)=>{
+    res.sendFile(__dirname + '/Error.html')
+})
+
+app.listen(3000,()=>{
+    console.log("Start Server at Port 3000")
 })
